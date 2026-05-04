8ae169ba54a113c7a8a41ec2ef4c6e3a9fc23755
ShawnBox <shawn_liu@seu.edu.cn>
2026-05-04 18:26:17 +0800

    feat: 性能优化 - 向量化PBC距离检查、修复try_count逻辑、VdW惰性加载
    - 消除 ex_atoms 27 倍膨胀，改用 numpy 向量化一次计算全部最小镜像距离
    - 移除 add_new_ex_atoms() 和相关存储，add_molecule 速度大幅提升
      - 每个 AddMol 文件独立 try_count，互不影响
    - 单原子分子跳过无效旋转
     - VdW.ini 改为惰性加载，带 try/except，路径基于 __file__
      - 新增类型提示

--- src/AddMolecule.py
+++ src/AddMolecule.py
@@ -1,56 +1,60 @@
 import random
-from src.MoleculeData import Atom
+from src.MoleculeData import Atom, POSCAR, AddMol
 import numpy as np
+from typing import Sequence
 
-# Constants
-'''
-Here, we define the maximum number of tries to add a molecule to the system.
-Maybe we will change this value in the future.
-'''
 MAX_TRIES = 1000
 
-def get_random_pos(range):
-    x = random.uniform(range[0][0], range[0][1])
-    y = random.uniform(range[1][0], range[1][1])
-    z = random.uniform(range[2][0], range[2][1])
-    return [x, y, z]
 
-def get_random_angle():
-    return [random.uniform(0, 360), random.uniform(0, 360), random.uniform(0, 360)]
+def get_random_pos(range_: Sequence[list[float]]) -> list[float]:
+    return [random.uniform(range_[0][0], range_[0][1]),
+            random.uniform(range_[1][0], range_[1][1]),
+            random.uniform(range_[2][0], range_[2][1])]
 
-def AddMolecule(poscar, add_range, num_mol, addmol, const_dist):
-    r"""
+
+def get_random_angle() -> list[float]:
+    return [random.uniform(0, 360),
+            random.uniform(0, 360),
+            random.uniform(0, 360)]
+
+
+def AddMolecule(poscar: POSCAR,
+                add_range: Sequence[list[float]],
+                num_mol: list[int],
+                addmol: list[AddMol],
+                const_dist: float) -> bool:
+    """
     poscar: POSCAR object
     add_range: list of list, the range of x, y, z in direct coordinates
-    num_mol: list of int, the number of molecules you want to add
+    num_mol: list of int, the number of molecules you want to add per AddMol
     addmol: list of AddMol object
     const_dist: float, the constant for distance calculation
     """
-
-    try_count = 0
     for i in range(len(num_mol)):
-        add_count = 0
-        while True:
-            if try_count == MAX_TRIES:
-                return False
-            if add_count == num_mol[i]:
-                break
+        added = 0
+        tries = 0
+        single = addmol[i].single_atom
+
+        while added < num_mol[i] and tries < MAX_TRIES:
             pos = np.dot(get_random_pos(add_range), poscar.box)
-            angle = get_random_angle()
+            angle = [0, 0, 0] if single else get_random_angle()
             coords = addmol[i].rotate(angle)
-            atoms = []
-            for k in range(addmol[i].num_mol):
-                atoms.append(Atom(addmol[i].elements[k], coords[k] + pos, ['T', 'T', 'T']))
+
+            atoms = [
+                Atom(addmol[i].elements[k], coords[k] + pos, ['T', 'T', 'T'])
+                for k in range(addmol[i].num_mol)
+            ]
 
             if not poscar.add_molecule(atoms, const_dist):
+                tries += 1
                 continue
-            add_count += 1
-            try_count += 1
-            print(f'Successfully added {add_count} molecules of the {i+1}th AddMol when trying {try_count} times')
-        
+
+            added += 1
+            tries += 1
+            print(f'  [{added}/{num_mol[i]}] Added molecule from AddMol #{i+1}'
+                  f' (trial {tries})')
+
+        if added < num_mol[i]:
+            return False
+
     return True
-        
-
-
-
-        
--- src/MoleculeData.py
+++ src/MoleculeData.py
@@ -1,250 +1,269 @@
 import numpy as np
 import copy
+import os
+from typing import Optional
 
-# read VdW.ini file
-Vdw = {}
-with open('./src/config/VdW.ini', 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        tmp = line.split()
-        key = tmp[0]
-        value = tmp[2]
-        Vdw[key] = float(value)
+# ---- VdW radii loading ----
 
-class POSCAR():
-    def __init__(self, file_path):
+_Vdw: Optional[dict[str, float]] = None
+
+
+def _load_vdw() -> dict[str, float]:
+    """Lazy-load van der Waals radii from config file."""
+    global _Vdw
+    if _Vdw is not None:
+        return _Vdw
+    _Vdw = {}
+    path = os.path.join(os.path.dirname(__file__), 'config', 'VdW.ini')
+    try:
+        with open(path, 'r') as f:
+            for line in f:
+                parts = line.split()
+                if len(parts) >= 3:
+                    _Vdw[parts[0]] = float(parts[2])
+    except (FileNotFoundError, ValueError):
+        pass  # proceed with empty dict
+    return _Vdw
+
+
+def _get_vdw(element: str) -> float:
+    return _load_vdw().get(element, 2.0)
+
+
+# ---- Helper: PBC-aware minimum image distance ----
+
+def _minimum_image_diff(coords_a: np.ndarray, coords_b: np.ndarray,
+                        box: np.ndarray, inv_box: np.ndarray) -> np.ndarray:
+    """
+    Vectorised minimum-image distance vectors.
+    coords_a: (N, 3) or (3,)   coords_b: (M, 3) or (3,)
+    Returns (N, M, 3) or (N or M, 3) distance vectors.
+    """
+    a, b = np.atleast_2d(coords_a), np.atleast_2d(coords_b)
+    diff_cart = a[:, None] - b[None, :]          # (N, M, 3)
+    diff_frac = diff_cart @ inv_box               # (N, M, 3)
+    diff_frac -= np.round(diff_frac)              # wrap to [-0.5, 0.5]
+    return diff_frac @ box                        # (N, M, 3)
+
+
+# ---- Classes ----
+
+class Atom:
+    def __init__(self, element: str, coordinates: list | np.ndarray,
+                 dynamic_label: list | None = None):
+        self.element = element
+        self.coordinates = np.asarray(coordinates, dtype=float)
+        self.dynamic_label = list(dynamic_label or [])
+        self.Vdw = _get_vdw(element)
+
+    def __str__(self) -> str:
+        return (f'{self.element}: {self.coordinates[0]} '
+                f'{self.coordinates[1]} {self.coordinates[2]} '
+                f'{self.dynamic_label}')
+
+    def is_legal(self, atom: 'Atom', const_dist: float = 0.5,
+                 box: np.ndarray | None = None,
+                 inv_box: np.ndarray | None = None) -> bool:
+        """Check whether *atom* is too close (respecting PBC if *box* given)."""
+        if box is not None and inv_box is not None:
+            diff = _minimum_image_diff(self.coordinates, atom.coordinates,
+                                       box, inv_box)
+            dist = np.linalg.norm(diff.ravel())
+        else:
+            dist = np.linalg.norm(self.coordinates - atom.coordinates)
+        return dist >= (self.Vdw + atom.Vdw) * const_dist
+
+
+class POSCAR:
+    def __init__(self, file_path: str):
         self.file_path = file_path
-        self.lattice_constant = 0
+        self.lattice_constant = 0.0
         self.box = np.zeros((3, 3))
-        self.elements = []
-
+        self.elements: list[str] = []
         self.dynamic = False
-        self.dynamic_label = []
-
+        self.dynamic_label: list[list[str]] = []
         self.direct = True
-        self.coordinates = []
-        self.atoms = []
-        self.element_dict = {}
-
-        self.ex_atoms = []
+        self.coordinates: list[np.ndarray] = []
+        self.atoms: list[Atom] = []
+        self.element_dict: dict[str, int] = {}
+        self._inv_box: np.ndarray | None = None
 
         self.read_POSCAR()
 
-    def read_POSCAR(self):
+    # ---- I/O ----
+
+    def read_POSCAR(self) -> None:
         with open(self.file_path, 'r') as f:
             lines = f.readlines()
-        
-        # read lattice constant
+
         self.lattice_constant = float(lines[1].strip())
 
-        # read box vectors
-        for i in range(2, 5):
-            self.box[i-2] = np.array([float(x) for x in lines[i].split()])
+        for i in range(3):
+            self.box[i] = np.array([float(x) for x in lines[i + 2].split()])
+        self._inv_box = np.linalg.inv(self.box)
 
-        # read elements
-        tmp_name = lines[5].split()
-        tmp_num = [int(lines[6].split()[k]) for k in range(len(tmp_name))]
-        for i, element in enumerate(tmp_name):
-            self.element_dict[element] = tmp_num[i]
+        # elements
+        names = lines[5].split()
+        counts = [int(lines[6].split()[k]) for k in range(len(names))]
+        self.element_dict = dict(zip(names, counts))
+        self.elements = [e for e, n in self.element_dict.items() for _ in range(n)]
 
-        def get_elements_idx(elements):
-            idx = []
-            for key, value in elements.items():
-                idx += [key] * value
-            return idx
-        self.elements = get_elements_idx(self.element_dict)
-
-        # remove information has been read
         lines = lines[7:]
+        if not lines or not lines[0].strip():
+            return
 
-        if lines[0].strip():
+        if lines[0].split()[0].lower() in ('s', 'selective', 'dynamics', 'd'):
+            self.dynamic = True
+            lines = lines[1:]
 
-            if  lines[0].split()[0].lower() in ['s', 'selective', 'dynamics', 'd']:
-                self.dynamic = True
-                lines = lines[1:]
+        if lines and lines[0].strip().lower() in ('c', 'cartesian'):
+            self.direct = False
 
-            # read direct or cartesian
-            if lines[0].strip().lower() in ['c', 'cartesian']:
-                self.direct = False
-
-            # read coordinates and dynamic label
-            for i in range(1, 1 + len(self.elements)):
-                self.coordinates.append([float(x) for x in lines[i].split()[:3]])
-                if self.dynamic:
-                    self.dynamic_label.append(lines[i].split()[3:])
-            
-        # make sure the coordinates are in cartesian
-        self.to_cartesian()
-
-        # get atoms
-        for i in range(len(self.elements)):
+        for i in range(1, 1 + len(self.elements)):
+            if i >= len(lines):
+                break
+            parts = lines[i].split()
+            self.coordinates.append(np.array([float(x) for x in parts[:3]]))
             if self.dynamic:
-                self.atoms.append(Atom(self.elements[i], self.coordinates[i], self.dynamic_label[i]))
-            else:
-                self.atoms.append(Atom(self.elements[i], self.coordinates[i]))
+                self.dynamic_label.append(parts[3:])
 
-        # get extra atoms considering the periodic boundary conditions
-        for atom in self.atoms:
-            self.add_new_ex_atoms(atom)
-
-    def to_cartesian(self):
-        if not self.direct:
-            return True
-        self.direct = False
-        for i in range(len(self.coordinates)):
-            self.coordinates[i] = np.dot(self.coordinates[i], self.box)
-        for atom in self.atoms:
-            atom.coordinates = np.dot(atom.coordinates, self.box)
-        for ex_atom in self.ex_atoms:
-            ex_atom.coordinates = np.dot(ex_atom.coordinates, self.box)
-        return True
-    
-    def to_direct(self):
-        if self.direct:
-            return True
-        self.direct = True
-        for i in range(len(self.coordinates)):
-            self.coordinates[i] = np.dot(self.coordinates[i], np.linalg.inv(self.box))
-        for atom in self.atoms:
-            atom.coordinates = np.dot(atom.coordinates, np.linalg.inv(self.box))
-        for ex_atom in self.ex_atoms:
-            ex_atom.coordinates = np.dot(ex_atom.coordinates, np.linalg.inv(self.box))
-        return True
-
-    def add_molecule(self, new_atoms, const_dist=0.5):
-        '''
-        Here, we should note that when the number of atoms in the new molecule is large, the time complexity of this function is O(nm) (m is the number of ex_atom, n is the number of new_atoms), which is not efficient.
-        Maybe we can use the k-d tree to optimize this function, so the time complexity can be reduced to O(nlogm).
-        '''
-        # TODO: using k-d tree to optimize this function
         self.to_cartesian()
-        for ex_atom in self.ex_atoms:
-            for new_atom in new_atoms:
-                if not ex_atom.is_legal(new_atom, const_dist):
-                    return False
 
-        self.atoms += new_atoms
-        self.elements += [new_atom.element for new_atom in new_atoms]
-        self.coordinates += [new_atom.coordinates for new_atom in new_atoms]
-        
-        for new_atom in new_atoms:
-            self.add_new_ex_atoms(new_atom)
+        # build atoms
+        for i in range(len(self.elements)):
+            coord = self.coordinates[i]
+            label = self.dynamic_label[i] if self.dynamic else []
+            self.atoms.append(Atom(self.elements[i], coord, label))
 
-        if self.dynamic:
-            self.dynamic_label += [new_atom.dynamic_label for new_atom in new_atoms]
-
-        for new_atom in new_atoms:
-            if new_atom.element in self.element_dict:
-                self.element_dict[new_atom.element] += 1
-            else:
-                self.element_dict[new_atom.element] = 1
-        return True
-
-    def write_POSCAR(self, file_path):
+    def write_POSCAR(self, file_path: str) -> None:
         self.to_direct()
         with open(file_path, 'w') as f:
             f.write('POSCAR generated by MolCraft\n')
             f.write(str(self.lattice_constant) + '\n')
             for i in range(3):
-                f.write(f'\t{self.box[i][0]:.10f}\t{self.box[i][1]:.10f}\t{self.box[i][2]:.10f}\n')
-
-            for key in self.element_dict.keys():
+                f.write(f'\t{self.box[i][0]:.10f}\t{self.box[i][1]:.10f}\t'
+                        f'{self.box[i][2]:.10f}\n')
+            for key in self.element_dict:
                 f.write(f'{key} ')
             f.write('\n')
-            for value in self.element_dict.values():
-                f.write(f'{value} ')
+            for val in self.element_dict.values():
+                f.write(f'{val} ')
             f.write('\n')
-
             if self.dynamic:
                 f.write('Selective Dynamics\n')
-
             f.write('Direct\n')
-            for key in self.element_dict.keys():
+            for key in self.element_dict:
                 for atom in self.atoms:
                     if atom.element == key:
-                        f.write(f'\t{atom.coordinates[0]:.10f}\t{atom.coordinates[1]:.10f}\t{atom.coordinates[2]:.10f}')
+                        f.write(f'\t{atom.coordinates[0]:.10f}'
+                                f'\t{atom.coordinates[1]:.10f}'
+                                f'\t{atom.coordinates[2]:.10f}')
                         if self.dynamic:
                             f.write(' ' + ' '.join(atom.dynamic_label))
                         f.write('\n')
 
-    def add_new_ex_atoms(self, atom):
-        def is_edge(coordinate):
-            for i in range(3):
-                if coordinate[i] < -0.3 or coordinate[i] > 1.3:
-                    return False
-            return True
+    # ---- Coordinate transforms ----
 
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                for k in range(-1, 2):
-                    new_coordinates = atom.coordinates + np.dot(np.array([i, j, k]), self.box)
-                    if not is_edge(np.dot(new_coordinates, np.linalg.inv(self.box))):
-                        continue
-                    self.ex_atoms.append(Atom(atom.element, new_coordinates))
-
-    def copy(self):
-        return copy.deepcopy(self)
-            
-
-
-class Atom():
-    def __init__(self, element, coordinates, dynamic_label=[]):
-        self.element = element
-        self.coordinates = coordinates
-        self.dynamic_label = dynamic_label
-        self.Vdw = Vdw[element]
-    
-    def __str__(self):
-        return f'{self.element}: {self.coordinates[0]} {self.coordinates[1]} {self.coordinates[2]} {self.dynamic_label}'
-
-    def is_legal(self, atom, const_dist=0.5):
-        if np.linalg.norm(np.array(self.coordinates) - np.array(atom.coordinates)) < (self.Vdw + atom.Vdw) * const_dist:
+    def to_cartesian(self) -> bool:
+        if not self.direct:
             return False
+        self.direct = False
+        for i, coord in enumerate(self.coordinates):
+            self.coordinates[i] = coord @ self.box
+        for atom in self.atoms:
+            atom.coordinates = atom.coordinates @ self.box
         return True
 
+    def to_direct(self) -> bool:
+        if self.direct:
+            return False
+        self.direct = True
+        inv = self._inv_box if self._inv_box is not None else np.linalg.inv(self.box)
+        for i, coord in enumerate(self.coordinates):
+            self.coordinates[i] = coord @ inv
+        for atom in self.atoms:
+            atom.coordinates = atom.coordinates @ inv
+        return True
 
-class AddMol():
-    def __init__(self, file_path):
+    # ---- Core logic ----
+
+    def add_molecule(self, new_atoms: list[Atom], const_dist: float = 0.5) -> bool:
+        """
+        Add *new_atoms* if none are within the vdW-radius distance threshold
+        of existing atoms (respecting periodic boundary conditions).
+        Uses vectorised numpy to check all pairs at once.
+        """
+        self.to_cartesian()
+
+        if not self.atoms:
+            self._append_atoms(new_atoms)
+            return True
+
+        existing = np.array([a.coordinates for a in self.atoms])   # (N, 3)
+        existing_vdw = np.array([a.Vdw for a in self.atoms])       # (N,)
+        inv = self._inv_box if self._inv_box is not None else np.linalg.inv(self.box)
+
+        for new_atom in new_atoms:
+            diff = _minimum_image_diff(existing, new_atom.coordinates,
+                                       self.box, inv).ravel()       # (N,)
+            dists = np.linalg.norm(diff.reshape(-1, 3), axis=1)    # (N,)
+            limits = (existing_vdw + new_atom.Vdw) * const_dist
+            if np.any(dists < limits):
+                return False
+
+        self._append_atoms(new_atoms)
+        return True
+
+    def _append_atoms(self, new_atoms: list[Atom]) -> None:
+        self.atoms += new_atoms
+        self.elements += [a.element for a in new_atoms]
+        self.coordinates += [a.coordinates for a in new_atoms]
+        if self.dynamic:
+            self.dynamic_label += [a.dynamic_label for a in new_atoms]
+        for a in new_atoms:
+            self.element_dict[a.element] = self.element_dict.get(a.element, 0) + 1
+
+    def copy(self) -> 'POSCAR':
+        return copy.deepcopy(self)
+
+
+class AddMol:
+    def __init__(self, file_path: str):
         self.file_path = file_path
         self.num_mol = 0
-        self.elements = []
-        self.coordinates = []
+        self.elements: list[str] = []
+        self.coordinates: np.ndarray = np.empty((0, 3))
+        self.single_atom = False
 
         self.read_AddMol()
-        self.geometric_center = self.geometric_center()
-        self.coordinates = self.new_coordinates(self.geometric_center)
-    
-    def read_AddMol(self):
+        centre = self.coordinates.mean(axis=0)
+        self.coordinates = self.coordinates - centre
+
+    def read_AddMol(self) -> None:
         with open(self.file_path, 'r') as f:
             lines = f.readlines()
-        
         self.num_mol = int(lines[0].strip())
-
+        self.single_atom = (self.num_mol == 1)
+        coords = []
         for i in range(1, 1 + self.num_mol):
-            self.coordinates.append([float(x) for x in lines[i].split()[:3]])
-            self.elements.append(lines[i].split()[3])
+            parts = lines[i].split()
+            coords.append([float(x) for x in parts[:3]])
+            self.elements.append(parts[3])
+        self.coordinates = np.array(coords, dtype=float)
 
-    # to calculate the geometric center of the added molecules
-    def geometric_center(self):
-        return np.mean(self.coordinates, axis=0)
-    
-    # to calculate new coordinates of the added molecules
-    def new_coordinates(self, center):
-        return np.array(self.coordinates) - center
-    
-    # to calculate new coordinates after rotation
-    def rotate(self, angle_list):
-        angle_list = np.array(angle_list) * np.pi / 180
-        matrix_x = np.array([[1, 0, 0], [0, np.cos(angle_list[0]), -np.sin(angle_list[0])], [0, np.sin(angle_list[0]), np.cos(angle_list[0])]])
-        matrix_y = np.array([[np.cos(angle_list[1]), 0, np.sin(angle_list[1])], [0, 1, 0], [-np.sin(angle_list[1]), 0, np.cos(angle_list[1])]])
-        matrix_z = np.array([[np.cos(angle_list[2]), -np.sin(angle_list[2]), 0], [np.sin(angle_list[2]), np.cos(angle_list[2]), 0], [0, 0, 1]])
-        matrix = np.dot(matrix_x, np.dot(matrix_y, matrix_z))
-        return np.dot(self.coordinates, matrix)
-
-
-if __name__ == '__main__':
-    poscar = POSCAR('./POSCAR')
-    poscar.to_cartesian()
-    poscar.write_POSCAR('./POSCAR_cartesian')
-    poscar.to_direct()
-    poscar.write_POSCAR('./POSCAR_direct')
+    def rotate(self, angle_list: list[float]) -> np.ndarray:
+        """Rotate coordinates by Euler angles (degrees) around geometric centre."""
+        a = np.radians(angle_list)
+        cx, sx = np.cos(a[0]), np.sin(a[0])
+        cy, sy = np.cos(a[1]), np.sin(a[1])
+        cz, sz = np.cos(a[2]), np.sin(a[2])
+        Rx = np.array([[1, 0, 0],
+                       [0, cx, -sx],
+                       [0, sx,  cx]])
+        Ry = np.array([[cy, 0, sy],
+                       [0, 1, 0],
+                       [-sy, 0, cy]])
+        Rz = np.array([[cz, -sz, 0],
+                       [sz,  cz, 0],
+                       [0, 0, 1]])
+        return self.coordinates @ (Rx @ Ry @ Rz)
